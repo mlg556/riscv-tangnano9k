3c55042956b95274847c33409597c7109f4632a6
Mirac Gulgonul <mlg556@gmail.com>
2023-09-17 22:47:54 +0300

    toolchain shenigans

--- src/blinker.s
+++ src/blinker.s
@@ -9,18 +9,22 @@
 
 main:
 .L0:
-	li   t0, 5
-	sw   t0, IO_LEDS(gp)
-	call wait
-	li   t0, 10
-	sw   t0, IO_LEDS(gp)
-	call wait
-	j .L0
+    li  t0, 5
+	sw  t0, IO_LEDS(gp)
+	li  a0, 104 # h
+    call putchar
+	call    wait
+	li  t0, 10
+	sw  t0, IO_LEDS(gp)
+    li  a0, 105 # i
+    call putchar
+	call    wait
+	j   .L0
 
 wait:
-	li t0,1
-	slli t0, t0, 19
+	li  t0,1
+	slli    t0, t0, 19
 .L1:       
-	addi t0,t0,-1
-	bnez t0, .L1
+	addi    t0,t0,-1
+	bnez    t0, .L1
 	ret
