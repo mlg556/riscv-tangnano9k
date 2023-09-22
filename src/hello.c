0c9b1ccdd2fb15fb01789f96da0c8cfdc85bc424
Mirac Gulgonul <mlg556@gmail.com>
2023-09-22 20:48:05 +0300

    hello world in C

--- src/hello.c
+++ src/hello.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
+void putstring(const char* s);
+void sleep(int x);
 
 int main(void) {
-    for(;;) {
-        printf("Hello world");
+    for (;;) {
+        putstring("Hello world\n");
+        sleep(10000);
     }
-
     return 0;
+}
+
+void sleep(int x) {
+    for (int i = 0; i < x; i++) {
+    }
 }
